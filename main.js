75c475b42c0d277e6f4d0f4b35ccc3327d1cdd2e
Andrew Teece <andrewteece@gmail.com>
2023-05-27 16:40:25 -0500

    slider/carousel

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ navToggle.addEventListener("click", () => {
     primaryHeader.toggleAttribute("data-overlay");
 });
 
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: false
+  });
+
