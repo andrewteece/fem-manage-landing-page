7d6c8ae6a5ee24d058e7eb73ee2f924524220e2f
Andrew Teece <andrewteece@gmail.com>
2023-05-27 17:22:05 -0500

    update js

--- main.js
+++ main.js
@@ -11,7 +11,14 @@ navToggle.addEventListener("click", () => {
 });
 
 const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: true,
-    dots: false
-  });
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+        480: {
+            dots: false, // dots enabled 1280px and up
+        },
+    },
+});
 
